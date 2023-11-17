c5f54917018db6c13087ea3c5ab15208e914d705
Truong Minh Danh <151100650+MinhDanh2005@users.noreply.github.com>
2023-11-17 14:56:26 +0700

    NgonNguLapTrinh

---
+++ jdoodle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello Toi ten la Danh\n");
+    int age = 18;
+    float weight = 48.5;
+    printf("Toi nam nay %d tuoi. Toi nang %.f kg" , age, weight);
+    return 0;
+}
